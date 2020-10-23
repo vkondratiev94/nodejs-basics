38912476c33fcb187614d8c9897313bca7d06e68
vkondratiev94 <vkondratiev94@gmail.com>
2020-10-24 00:19:07 +0300

    sending responses

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("req", req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from Node.js server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3001);

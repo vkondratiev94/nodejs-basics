c63ef94b0c9223147466147cdaa8493e05fbb126
vkondratiev94 <vkondratiev94@gmail.com>
2020-10-24 00:49:19 +0300

    routing requests

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("req", req.url, req.method, req.headers);
-
+  const url = req.url;
   res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from Node.js server!</h1></body>");
-  res.write("</html>");
+
+  switch (url) {
+    case "/":
+      res.write("<html>");
+      res.write("<head><title>Enter message</title></head>");
+      res.write(
+        `<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>`
+      );
+      res.write("</html>");
+      break;
+
+    default:
+      res.write("<html>");
+      res.write("<head><title>My First Page</title></head>");
+      res.write("<body><h1>Hello from Node.js server!</h1></body>");
+      res.write("</html>");
+  }
+
   res.end();
 });
 

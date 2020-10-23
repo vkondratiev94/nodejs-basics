1f97c26105ce875ab17620865764087867f5620c
vkondratiev94 <vkondratiev94@gmail.com>
2020-10-24 00:55:39 +0300

    redirecting requests

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  res.setHeader("Content-Type", "text/html");
+  const method = req.method;
 
-  switch (url) {
-    case "/":
+  switch (true) {
+    case url === "/":
       res.write("<html>");
       res.write("<head><title>Enter message</title></head>");
       res.write(
         `<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>`
       );
       res.write("</html>");
+      break;
+
+    case url === "/message" && method === "POST":
+      fs.writeFileSync("message.txt", "Dummy text");
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
       break;
 
     default:

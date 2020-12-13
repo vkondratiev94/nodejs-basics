4f0276b80c6a6cb61df3efd39e5fa0095f6b423e
vkondratiev94 <vkondratiev94@gmail.com>
2020-12-13 23:15:30 +0200

    filtering paths

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(shopRoutes)
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 
 app.use((req, res, next) => {
   res.status(404).send('<h1>Page not found</h1>')

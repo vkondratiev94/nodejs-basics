113f809a3859600eb20d5b0e4eff0650c4cefafa
vkondratiev94 <vkondratiev94@gmail.com>
2020-12-13 21:39:58 +0200

    Adding a 404 error page

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(shopRoutes)
 app.use(adminRoutes)
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
+
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`)
